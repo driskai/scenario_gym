c0a10205e240f155cafbe2b10d35299f50cf7cd5
ib2953 <ibrant2953@gmail.com>
2022-09-23 16:34:05 +0100

    Added RSS and RSSDistances to metrics __init__

--- scenario_gym/metrics/__init__.py
+++ scenario_gym/metrics/__init__.py
@@ -1,5 +1,6 @@
 from scenario_gym.metrics.base import Metric, cache_mean, cache_metric
 from scenario_gym.metrics.collision import CollisionMetric
+from scenario_gym.metrics.rss.rss import RSS, RSSDistances
 from scenario_gym.metrics.trajectory import (
     EgoAvgSpeed,
     EgoDistanceTravelled,
