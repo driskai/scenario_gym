3c4a519815981531d2740321de3f1da754c275d9
Hamish Scott <41787553+hamishs@users.noreply.github.com>
2022-10-04 09:45:12 +0100

    Rendering-2 (#8)
    * refactor cv2 viewer
    
    * cache coords for speed
    
    * add arg for entity_front_color
    
    * remove _2 in filepath

--- scenario_gym/viewer/base.py
+++ scenario_gym/viewer/base.py
@@ -13,7 +13,20 @@ class Viewer(ABC):
     which will render the rollout and then be destroyed.
     """
 
-    def __init__(self, output_path: Optional[str]):
+    def __init__(self):
+        self.output_path: Optional[str] = None
+
+    @abstractmethod
+    def reset(self, output_path: str) -> None:
+        """
+        Reset the viewer at the start of a new scenario rollout.
+
+        Parameters
+        ----------
+        output_path : str
+            Path for the output video file.
+
+        """
         self.output_path = output_path
 
     @abstractmethod
@@ -22,6 +35,12 @@ class Viewer(ABC):
         Render the current state of the environment.
 
         Should optionally return the current key press.
+
+        Parameters
+        ----------
+        state : State
+            Current global state of the gym.
+
         """
         raise NotImplementedError
 
